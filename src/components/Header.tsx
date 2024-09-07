12a51e74bff38aab5c47c86f8ad9bccf169c85ad
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-09-07 16:37:29 -0500

    add: agregar formulario de busqueda

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,12 @@
-import { NavLink } from "react-router-dom"
+import { useMemo } from "react"
+import { NavLink, useLocation } from "react-router-dom"
 
 export const Header = () => {
+
+    const { pathname } = useLocation()
+
+    const isHome = useMemo(() => pathname === '/', [pathname])
+
     return (
         <header className="bg-slate-800">
             <div className="mx-auto container px-5 py-16">
@@ -19,6 +25,46 @@ export const Header = () => {
                         >Favoritos</NavLink>
                     </nav>
                 </div>
+                {isHome && (
+                    <form className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+                        <div className="space-y-4">
+                            <label
+                                htmlFor="ingredient"
+                                className="block text-white uppercase font-extrabold text-lg"
+                            >Nombre o Ingredientes
+                            </label>
+                            <input
+                                id="ingredient"
+                                type="text"
+                                name="ingredient"
+                                className="p-3 w-full rounded-lg focus:outline-none"
+                                placeholder="Nombre o ingrediente"
+                            />
+                        </div>
+                        <div className="space-y-4">
+                            <label
+                                htmlFor="category"
+                                className="block text-white uppercase font-extrabold text-lg"
+                            >Categoria
+                            </label>
+                            <select
+                                id="category"
+                                name="ingrcategoryedient"
+                                className="p-3 w-full rounded-lg focus:outline-none"
+                            >
+                                <option>Seleccione:</option>
+                            </select>
+                        </div>
+                        <input
+                            type="submit"
+                            value='Buscar Receta'
+                            className="cursor-pointer bg-white text-orange-400 uppercase font-bold p-3 rounded-lg w-full
+                            hover:bg-orange-300 hover:text-white"
+                        />
+
+                    </form>
+                )
+                }
             </div>
         </header>
     )
