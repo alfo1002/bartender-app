29f6c6189a8bfa4ebfa5690ebbf54ac829cfc0e0
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-09-09 17:20:51 -0500

    add: mostrar recetas en seccion favoritos

--- src/views/FavoritesPage.tsx
+++ src/views/FavoritesPage.tsx
@@ -1,6 +1,30 @@
+import { useMemo } from "react"
+import { DrinkCard } from "../components/DrinkCard"
+import { useAppStore } from "../stores/useAppStore"
+
 
 export const FavoritesPage = () => {
+
+    const favorites = useAppStore(state => state.favorites)
+    const hasFavorites = useMemo(() => favorites.length, [favorites])
+
     return (
-        <div>FavoritesPage</div>
+        <>
+            <h1 className="text-9xl font-bold">Favoritos</h1>
+
+            {hasFavorites ? (
+                <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
+                    {favorites.map(drink => (
+                        <DrinkCard
+                            key={drink.idDrink}
+                            drink={drink}
+                        />
+                    ))}
+                </div>
+            ) : (
+                <p className="my-10 text-center text-2xl">No tienes favoritos aún</p>
+            )}
+
+        </>
     )
 }
