88eb4e2a5f3bb814b1a5f01c2c78ed19b61bf2dc
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-09-09 11:11:27 -0500

    add: mostrar información de API en una CARD

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
-import { CategoriesAPIResponseSchema, DrinksAPIResponseSchema, SearchFilterSchema } from '../utils/receipes-schema';
+import { CategoriesAPIResponseSchema, DrinkAPIResponseSchema, DrinksAPIResponseSchema, SearchFilterSchema } from '../utils/receipes-schema';
 
 export type Categories = z.infer<typeof CategoriesAPIResponseSchema>;
 export type SearchFilter = z.infer<typeof SearchFilterSchema>
 export type Drinks = z.infer<typeof DrinksAPIResponseSchema>
+export type Drink = z.infer<typeof DrinkAPIResponseSchema>
